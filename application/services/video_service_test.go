c4b718c19a9fb92137d9aec6cbb93361ff212428
Deivid Roger Oliveira Santos <deivid@deividroger.net>
2021-09-18 12:18:21 -0300

    video service was finished

---
+++ application/services/video_service_test.go
@@ -0,0 +1,63 @@
+package services_test
+
+import (
+	"encoder/application/repositories"
+	"encoder/application/services"
+	"encoder/domain"
+	"encoder/framework/database"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/joho/godotenv"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+
+	if err != nil {
+		log.Fatalf("Erro loading .env file")
+	}
+}
+
+func prepare() (*domain.Video, repositories.VideoRepository) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "video1.mp4"
+	video.CreateAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+
+	repo.Insert(video)
+
+	return video, repo
+}
+
+func TestVideoServiceDownload(t *testing.T) {
+	video, repo := prepare()
+	videoService := services.NewVideoService()
+
+	videoService.Video = video
+	videoService.VideoRepository = &repo
+
+	err := videoService.Download("deivid-code-micro-videos")
+
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+
+	require.Nil(t, err)
+
+	err = videoService.Encode()
+
+	require.Nil(t, err)
+
+	err = videoService.Finish()
+
+	require.Nil(t, err)
+}
--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module encoder
 go 1.14
 
 require (
+	cloud.google.com/go/storage v1.16.1
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
 	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.1.1
 	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.8.1
-	github.com/stretchr/testify v1.2.2
+	github.com/stretchr/testify v1.6.1
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
